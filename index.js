c09bdcaeccfe7f287a3f073040f19c1b036f6928
Akshay Kumar Hiran <akshaykumarhiran2@gmail.com>
2024-08-12 18:33:29 +0530

    Work on Seller Panel and sidebar

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ io.on("connection", (socket) => {
 app.listen(PORT, "0.0.0.0", () => {
     // setTimeout(printURL, 50);
     console.log(printURL());
-    rainbow(`App running on port ${PORT} ..`);
+    // rainbow(`App running on port ${PORT} ..`);
 });
 
 const printURL = () => {
