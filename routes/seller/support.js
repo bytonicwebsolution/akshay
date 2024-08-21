4c5b7b5ec04b96b98f6bd6e2b1c024ecb4433ca8
Akshay Kumar Hiran <akshaykumarhiran2@gmail.com>
2024-08-21 19:02:07 +0530

    work on support in seller

--- routes/seller/support.js
+++ routes/seller/support.js
@@ -3,5 +3,7 @@ const router = require("express").Router();
 const SupportController = require("../../controllers/seller/supportController");
 
 router.get("/list", SupportController.list);
+router.post("/add", SupportController.add);
+router.post("/delete/:id", SupportController.delete);
 
 module.exports = router;
